24265cda32da30a847db1b4b8104f034c33c0cfe
Lars Bergmann <lars.bergmann@perfact.de>
2020-07-13 01:50:45 +0200

    Player instancing and controll select before start

--- DemoScene.gd
+++ DemoScene.gd
@@ -1,21 +1,13 @@
+class_name BaseScene
+
 extends Spatial
 
 var finished_count = 0
-var players_init = true
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var players_init = false
 
 func player_selection():
 	get_tree().current_scene.get_node("UI/PlayerSelectionNotice").popup_centered()
 	get_tree().paused = true
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	# player_selection()
-	return
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	player_selection()
--- UI.gd
+++ UI.gd
@@ -1,27 +1,46 @@
 extends Control
 
+var player_prefixes = ["", "p2_"]
+var instanced_prefixes = []
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func create_player(prefix):
+	if prefix in instanced_prefixes:
+		return
 
+	var scene = load("res://player/player.tscn")
+	var item_node = scene.instance()
+	var ItemContainer = get_parent()
+	var ItemSpawner = get_parent().get_node("PlayerSpawn")
+	ItemContainer.add_child(item_node)
+	item_node.set_translation(ItemSpawner.translation)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+	item_node.player_prefix = prefix
+	item_node.rotate_y(-PI/2)
+
+	instanced_prefixes.append(prefix)
 
 func _input(event):
 
-	if Input.is_action_just_pressed("pause_menu"):
-		if !get_tree().current_scene.players_init:
-			return
-		if !get_tree().paused:
-			get_tree().current_scene.get_node("UI/Popup").popup_centered()
-			get_tree().paused = true
-		else:
-			get_tree().current_scene.get_node("UI/Popup").hide()
-			get_tree().paused = false
+	if !get_tree().current_scene.players_init:
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		for prefix in player_prefixes:
+			if Input.is_action_just_pressed(prefix + "pause_menu"):
+				create_player(prefix)
+				get_tree().current_scene.players_init = true
+				get_node("PlayerSelectionNotice").hide()
+				get_tree().paused = false
+	else:
+
+		for prefix in player_prefixes:
+
+			if Input.is_action_just_pressed(prefix + "pause_menu"):
+
+				if !(prefix in instanced_prefixes):
+					create_player(prefix)
+				else:
+					if !get_tree().paused:
+						get_tree().current_scene.get_node("UI/Popup").popup_centered()
+						get_tree().paused = true
+					else:
+						get_tree().current_scene.get_node("UI/Popup").hide()
+						get_tree().paused = false
