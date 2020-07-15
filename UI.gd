1f5537aff1a3cd5a0b93445686e17dd767c9035b
Lars Bergmann <lars.bergmann@perfact.de>
2020-07-15 17:24:29 +0200

    Add a panel to show possible player interactions

--- UI.gd
+++ UI.gd
@@ -2,6 +2,7 @@ extends Control
 
 var player_prefixes = ["", "p2_"]
 var instanced_prefixes = []
+var player_instace_paths = []
 
 func create_player(prefix):
 	if prefix in instanced_prefixes:
@@ -16,8 +17,36 @@ func create_player(prefix):
 
 	item_node.player_prefix = prefix
 	item_node.rotate_y(-PI/2)
+	player_instace_paths.append(item_node.get_path())
 
 	instanced_prefixes.append(prefix)
+
+func _process(delta):
+	var player_panel_container = get_node("PlayerPanel/HBoxContainer")
+	for item in player_panel_container.get_children():
+		item.queue_free()
+
+	for player_path in player_instace_paths:
+		var player_node = get_node(player_path)
+		var player_msg = '# ' + player_node.name
+		if player_node.carried_object:
+			player_msg += 'Holding Item: '
+			player_msg += player_node.carried_object.name
+		if player_node.transfer_slot:
+			player_msg += ' Facing CraftingSpot: '
+			player_msg += player_node.transfer_slot.name
+			player_msg += ' (' + player_node.transfer_slot.recipe_item_node.name + ')'
+			if player_node.transfer_slot.can_craft:
+				player_msg += ' (READY)'
+		if player_node.pickitem:
+			player_msg += ' Can pick up item: '
+			player_msg += player_node.pickitem.name
+		if player_node.interact_object:
+			player_msg += ' Can interact with: '
+			player_msg += player_node.interact_object.name
+		var player_label = Label.new()
+		player_label.text = player_msg
+		player_panel_container.add_child(player_label)
 
 func _input(event):
 
